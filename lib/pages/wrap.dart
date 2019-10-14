b1a447d6e4c8ed03e6c345f86d8dc9ff10978831
luwenxull <992273307@qq.com>
2019-10-14 15:12:37 +0800

    wrap

--- lib/pages/wrap.dart
+++ lib/pages/wrap.dart
@@ -1,57 +1,105 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class DemoWrap extends StatefulWidget {
+class WrapDemo extends StatefulWidget {
   @override
-  _DemoWrap createState() {
-    return _DemoWrap();
+  _WrapDemo createState() {
+    return _WrapDemo();
   }
 }
 
-class _DemoWrap extends State<DemoWrap> {
+class _WrapDemo extends State<WrapDemo> {
+  double spacing = 8.0;
+  double runSpacing = 4.0;
+  final spacingInputController = TextEditingController();
+  final runSpacingInputController = TextEditingController();
 
   update() {
     setState(() {
-
+      // TODO catch error
+      spacing = double.parse(spacingInputController.text);
+      runSpacing = double.parse(runSpacingInputController.text);
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Wrap(
-          spacing: 8.0, // gap between adjacent chips
-          runSpacing: 4.0, // gap between lines
-          children: <Widget>[
-            Chip(
-              avatar: CircleAvatar(
-                  backgroundColor: Colors.blue.shade900, child: Text('AH')),
-              label: Text('Hamilton'),
-            ),
-            Chip(
-              avatar: CircleAvatar(
-                  backgroundColor: Colors.blue.shade900, child: Text('ML')),
-              label: Text('Lafayette'),
-            ),
-            Chip(
-              avatar: CircleAvatar(
-                  backgroundColor: Colors.blue.shade900, child: Text('HM')),
-              label: Text('Mulligan'),
-            ),
-            Chip(
-              avatar: CircleAvatar(
-                  backgroundColor: Colors.blue.shade900, child: Text('JL')),
-              label: Text('Laurens'),
-            ),
-          ],
-        ),
-        Column(
-          children: <Widget>[
+  void initState() {
+    super.initState();
+    spacingInputController.text = spacing.toString();
+    runSpacingInputController.text = runSpacing.toString();
+  }
 
-          ],
+  @override
+  void dispose() {
+    super.dispose();
+    spacingInputController.dispose();
+    runSpacingInputController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        SizedBox(
+          // height: 300,
+          child: Wrap(
+            spacing: spacing, // gap between adjacent chips
+            runSpacing: runSpacing, // gap between lines
+            alignment: WrapAlignment.spaceAround, //主轴排列
+            crossAxisAlignment: WrapCrossAlignment.center, // 行内元素在副轴如何对齐（元素需要高度不同，高度相同时无效）
+            // runAlignment: WrapAlignment.center, // run在副轴如何对齐（需要parent有height constraint）
+            children: <Widget>[
+              Chip(
+                avatar: CircleAvatar(
+                    backgroundColor: Colors.blue.shade900, child: Text('AH')),
+                label: Text('Hamilton'),
+              ),
+              Card(
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  child: Center(child: Text('A card')),
+                ),
+              ),
+              Chip(
+                avatar: CircleAvatar(
+                    backgroundColor: Colors.blue.shade900, child: Text('HM')),
+                label: Text('Mulligan'),
+              ),
+              Chip(
+                avatar: CircleAvatar(
+                    backgroundColor: Colors.blue.shade900, child: Text('JL')),
+                label: Text('Laurens'),
+              ),
+            ],
+          ),
         ),
-        RaisedButton(child: Text('更新'),onPressed: update,)
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: spacingInputController,
+                decoration: InputDecoration(
+                  labelText: 'spacing',
+                  // border: OutlineInputBorder(),
+                ),
+              ),
+              TextField(
+                controller: runSpacingInputController,
+                decoration: InputDecoration(
+                  labelText: 'runSpacing',
+                  // border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+        ),
+        RaisedButton(
+          child: Text('更新'),
+          onPressed: update,
+          color: Theme.of(context).primaryColor,
+        )
       ],
     );
   }
